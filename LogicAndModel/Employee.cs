a72dcf35d58a0e410a1a47881ecdb7603b8347c7
AkEnZ1 <77202776+AkEnZ1@users.noreply.github.com>
2025-09-23 14:28:20 +0700

    Update Employee.cs

--- LogicAndModel/Employee.cs
+++ LogicAndModel/Employee.cs
@@ -19,6 +19,12 @@ namespace LogicAndModel
         public string Name { get; set; }
         public VacancyType Vacancy { get; set; }
 
+        /// <summary>
+        /// Возвращает строковое представление информации о сотруднике.
+        /// </summary>
+        /// <returns>
+        /// Строка в формате: "Имя: {Name}, Должность: {Vacancy}, Опыт: {WorkExp} лет".
+        /// </returns>
         public override string ToString()
         {
             return $"Имя: {Name}, Должность: {Vacancy}, Опыт: {WorkExp} лет";
